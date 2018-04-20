cd9297d282206715f2169151606173ced2517874
mvtercero <mvtercero85@gmail.com>
2018-04-20 12:18:59 +0200

    Tested arrays

--- jasmine/spec/appSpec.js
+++ jasmine/spec/appSpec.js
@@ -1,14 +1,23 @@
-describe("App", function(){
-    var app;
-
-    beforeEach(function(){
-       app =  getApplication();
-       app.start();
-    });
+    //Describe anidado
     
-    it("check random number exists", function() {
-        expect(app.getRandomIntInclusive(1,100)).toBeGreaterThanOrEqual(1);
-        expect(app.getRandomIntInclusive(1,100)).toBeLessThanOrEqual(100);
-    });
+    describe("Javascript Allonge", function() { 
+        describe("values and expressions", function() {
+            it("simple example operator +", function() {
+                expect("Maria" + " " + "Vedia").toEqual("Maria Vedia");
+            });
+        });
 
-})
+        describe("values and identity", function(){
+            var array1= [1,2];
+            var array2= [1,2];
+            
+            it("compares arrayes", function() {
+                expect([2-1, 2, 2+1] === [1,2,3]).toEqual(false);
+                expect("Estela").toEqual('Estela');
+                expect(array1 === array2).toEqual(false);
+                expect([1,2]).toEqual([1,2]);
+            });
+
+        });
+
+    });
